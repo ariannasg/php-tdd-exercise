2d06765c573f6ba1fdf7babbfab11e7a01a78bdb
Ari <ariannasg.ldn@gmail.com>
2020-05-06 12:00:28 +0100

    Added tests for postTaxTotal

--- src/Receipt.php
+++ src/Receipt.php
@@ -4,13 +4,31 @@ namespace TDD;
 
 class Receipt
 {
-    public function total(array $items = []): float
+    public function __construct()
     {
-        return (float)array_sum($items);
+    }
+
+    public function total(array $items = [], float $coupon = null): float
+    {
+        $sum = (float)array_sum($items);
+
+        if ($sum !== 0 && $coupon !== null) {
+            $sum -= ($sum * $coupon);
+        }
+
+        return $sum;
     }
 
     public function tax(float $amount, float $tax): float
     {
         return ($amount * $tax);
     }
+
+    public function postTaxTotal(array $amount, float $tax, float $coupon = null): float
+    {
+        $subtotal = $this->total($amount, $coupon);
+        return $subtotal + $this->tax($subtotal, $tax);
+    }
+
+
 }
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -25,12 +25,25 @@ class ReceiptTest extends TestCase
     public function testTotal(): void
     {
         $input = [0, 2, 5, 8];
-        $output = $this->receipt->total($input);
+        $coupon = null;
+        $output = $this->receipt->total($input, $coupon);
 
         self::assertEquals(
             15,
             $output,
             'When summing the total should equal 15');
+    }
+
+    public function testTotalAndCoupon(): void
+    {
+        $input = [0, 2, 5, 8];
+        $coupon = 0.20;
+        $output = $this->receipt->total($input, $coupon);
+
+        self::assertEquals(
+            12,
+            $output,
+            'When summing the total should equal 12');
     }
 
     public function testTax(): void
@@ -40,5 +53,16 @@ class ReceiptTest extends TestCase
         $output = $this->receipt->tax($inputAmount, $taxInput);
 
         self::assertEquals(1.00, $output, 'The tax calculation should equal 1.00');
+    }
+
+    public function testPostTaxTotal(): void
+    {
+        $receipt = $this->getMockBuilder(Receipt::class)->setMethods(['total', 'tax'])->getMock();
+        $receipt->method('total')->willReturn(10.00);
+        $receipt->method('tax')->willReturn(1.00);
+
+        $result = $receipt->postTaxTotal([1, 2, 5, 8], 0.20, null);
+
+        self::assertEquals(11.00, $result);
     }
 }
